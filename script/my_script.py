3e487f902f979fc72cf893b62bca1b641b74659a
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-24 16:25:57 +0000

    fixed step 1

--- script/my_script.py
+++ script/my_script.py
@@ -1,20 +1,44 @@
 import mne
-import matplotlib.pyplot as plt
-# mne.sys_info()
+import numpy as np
+
+raw = mne.io.read_raw_edf("S001R01.edf", preload=True)
+raw.filter(8, 30)
+raw.rename_channels(lambda x: x.replace("..", ""))
+
+sfreq = raw.info['sfreq']
+window_size = int(2 * sfreq)
+
+motor = raw.pick(["C3", "C4", "Cz"])
+
+data = motor.get_data()
 
 
-file_path = "S001R01.edf"
-mne.set_config('MNE_LOGGING_LEVEL', 'INFO', set_env=True)
-raw = mne.io.read_raw_edf(file_path,preload=True)
+windows = []
+step = window_size // 2
+for start in range(0, data.shape[1] - window_size, step):
+    stop = start + window_size
+    chunk = data[:, start:stop]   # (3, window_size)
+    windows.append(chunk)
 
-print(raw.ch_names)
-motor_channels = ["C3..", "C4..", "Cz.."]
+windows = np.array(windows)
 
-raw_filtered = raw.copy().filter(l_freq=8, h_freq=30)
-raw_motor = raw_filtered.copy().pick_channels(motor_channels)
+features = []
 
+for chunk in windows:
+    psd, freqs = mne.time_frequency.psd_array_welch(
+        chunk,
+        sfreq=sfreq,
+        fmin=8,
+        fmax=30,
+        verbose=False
+    )
+    psd = np.log(psd + 1e-12)
+    mu = psd[:, (freqs >= 8) & (freqs <= 12)].mean(axis=1)
+    beta = psd[:, (freqs >= 13) & (freqs <= 30)].mean(axis=1)
+    
+    feat = np.concatenate([mu, beta])
+    features.append(feat)
 
-raw.plot()
-raw_motor.plot()
-# print(raw_filtered.shape())
-plt.show()
+features = np.array(features)
+features_norm = ((features - features.mean(axis=0)) / features.std(axis=0))
+print(features_norm)
