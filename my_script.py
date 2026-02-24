64e4b4de7c6162fed9e9763957cd358bf19e9fd1
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-24 03:46:47 +0000

    filter raw 8 - 30

--- my_script.py
+++ my_script.py
@@ -7,11 +7,18 @@ file_path = "S001R01.edf"
 mne.set_config('MNE_LOGGING_LEVEL', 'INFO', set_env=True)
 raw = mne.io.read_raw_edf(file_path,preload=True)
 
-print(raw.ch_names)
-data, times = raw.get_data(picks='C3..', return_times=True)
+# print(raw.ch_names)
 
-plt.plot(times, data.T)
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.title('EEG Channel C3')
+raw_filtered = raw.copy().filter(l_freq=8, h_freq=30)
+
+
+raw.plot()
+raw_filtered.plot()
+# print(raw_filtered)
+
+# data, times = raw_filtered.get_data(picks='C3..', return_times=True)
+# plt.plot(times, data.T)
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
+# plt.title('EEG Channel C3')
 plt.show()
